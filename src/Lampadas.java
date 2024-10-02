1f4040eb74511c417d0fcd6b71c76405e0d1f39e
AlissonGSantos <alisson.gabriel@ufpr.br>
2024-10-02 15:48:42 -0300

    Exercício 5

---
+++ src/Lampadas.java
@@ -0,0 +1,17 @@
+public class Lampadas {
+    /*
+    Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes.
+    Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+    Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas
+    idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+
+    Resposta:
+    Para descobrir qual interruptor controla cada lâmpada com apenas uma ida à sala das lâmpadas, primeiro ligo o
+    primeiro interruptor e deixo-o ligado por alguns minutos. Em seguida, desligo o primeiro e ligo o segundo. Vou até a
+    sala das lâmpadas e observo: a lâmpada acesa está ligada ao segundo interruptor, a apagada, mas ainda quente, ao
+    primeiro, e a apagada e fria, ao terceiro, que eu não ligo. Assim, consigo identificar cada interruptor em uma única
+    visita.
+    */
+
+
+}
